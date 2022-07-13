78835603807d92a3311d9194e92df0418a339b7a
maybaby <ybyang7@iflytek.com>
2022-07-13 10:30:09 +0800

    doc: remove build
    Signed-off-by: maybaby <ybyang7@iflytek.com>

--- build/assets/js/390b5911.36d44274.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkathenaserving=self.webpackChunkathenaserving||[]).push([[822],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
