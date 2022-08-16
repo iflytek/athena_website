fb1c65997bcb662b6bd12115cedde77833d27bef
ybyang7 <ybyang7@iflytek.com>
2022-08-16 17:18:11 +0800

    doc: update
    Signed-off-by: ybyang7 <ybyang7@iflytek.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/介绍">
+            to="/docs/概览">
             Serving Tutorial - 5min ⏱️
           </Link>
         </div>
